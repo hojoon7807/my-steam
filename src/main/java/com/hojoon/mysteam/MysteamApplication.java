fb81fb60959665790b911eb9772f6b24d4a0a110
hojoon <ujuj0202@gmail.com>
2022-05-25 13:34:05 +0900

    초기 프로젝트 설정, jira 테스트

---
+++ src/main/java/com/hojoon/mysteam/MysteamApplication.java
@@ -0,0 +1,13 @@
+package com.hojoon.mysteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysteamApplication.class, args);
+	}
+
+}
