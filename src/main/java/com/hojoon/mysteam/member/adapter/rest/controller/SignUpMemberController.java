eabdb447125ff9659617b56f37a6f2e08741a01f
hojoon <ujuj0202@gmail.com>
2022-06-11 01:40:10 +0900

    [MYS-15] port -> usecase 네이밍 변경

--- src/main/java/com/hojoon/mysteam/member/adapter/rest/controller/SignUpMemberController.java
+++ src/main/java/com/hojoon/mysteam/member/adapter/rest/controller/SignUpMemberController.java
@@ -2,7 +2,7 @@ package com.hojoon.mysteam.member.adapter.rest.controller;
 
 import com.hojoon.mysteam.member.adapter.rest.dto.request.SignUpMemberRequest;
 import com.hojoon.mysteam.member.adapter.rest.dto.response.SignUpMemberResponse;
-import com.hojoon.mysteam.member.application.usecase.CreateMemberPort;
+import com.hojoon.mysteam.member.application.usecase.CreateMemberUsecase;
 import com.hojoon.mysteam.member.domain.model.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SignUpMemberController {
 
-  private final CreateMemberPort createMemberUseCase;
+  private final CreateMemberUsecase createMemberUseCase;
 
   @PostMapping("/signup")
   public ResponseEntity<SignUpMemberResponse> signUp(
