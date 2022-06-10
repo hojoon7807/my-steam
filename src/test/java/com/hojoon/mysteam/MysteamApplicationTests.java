75159fdf31ea2ab607130dc28951a99f171152ef
hojoon <ujuj0202@gmail.com>
2022-06-10 09:25:52 +0900

    [MYS-15] @IntegrationTest 커스텀 어노테이션 적용

--- src/test/java/com/hojoon/mysteam/MysteamApplicationTests.java
+++ src/test/java/com/hojoon/mysteam/MysteamApplicationTests.java
@@ -1,10 +1,12 @@
 package com.hojoon.mysteam;
 
+import com.hojoon.mysteam.common.AbstractContainerBaseTest;
+import com.hojoon.mysteam.common.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MysteamApplicationTests {
+@IntegrationTest
+class MysteamApplicationTests extends AbstractContainerBaseTest {
 
   @Test
   void contextLoads() {
