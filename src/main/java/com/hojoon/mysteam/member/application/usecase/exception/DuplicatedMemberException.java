9f9d4c5942f72874c0651c6214b0573d092b60b6
hojoon <ujuj0202@gmail.com>
2022-06-14 02:59:25 +0900

    [MYS-25] SignUpMemberController 테스트 코드 작성

--- src/main/java/com/hojoon/mysteam/member/application/usecase/exception/DuplicatedMemberException.java
+++ src/main/java/com/hojoon/mysteam/member/application/usecase/exception/DuplicatedMemberException.java
@@ -1,7 +1,10 @@
 package com.hojoon.mysteam.member.application.usecase.exception;
 
 import com.hojoon.mysteam.common.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DuplicatedMemberException extends BusinessException {
 
   public DuplicatedMemberException(String message) {
