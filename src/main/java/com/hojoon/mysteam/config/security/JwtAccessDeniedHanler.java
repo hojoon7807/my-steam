61702650a258ea552e04133356e5d467b5ea51e1
hojoon <ujuj0202@gmail.com>
2022-06-16 00:14:55 +0900

    [MYS-26] jwt entry point deniedHandler 구현

---
+++ src/main/java/com/hojoon/mysteam/config/security/JwtAccessDeniedHanler.java
@@ -0,0 +1,22 @@
+package com.hojoon.mysteam.config.security;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class JwtAccessDeniedHanler implements AccessDeniedHandler {
+
+  @Override
+  public void handle(HttpServletRequest request, HttpServletResponse response,
+      AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    log.error("accessDenied error : {}", accessDeniedException.getMessage());
+    response.sendError(HttpServletResponse.SC_FORBIDDEN);
+  }
+}
