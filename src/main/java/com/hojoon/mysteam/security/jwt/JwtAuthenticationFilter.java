579ca844c611e632568a120bf53285eb3244bf7b
hojoon <ujuj0202@gmail.com>
2022-06-23 23:34:14 +0900

    [MYS-26] signIn 구현
    controller->service동작 방식과 UsernamePasswordAuthenticationFilter를 직접구현 하는 2가지 방법으로 로그인을 구현했습니다.
    /api/signin 으로 로그인 요청

---
+++ src/main/java/com/hojoon/mysteam/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.hojoon.mysteam.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hojoon.mysteam.member.adapter.rest.dto.request.SignInMemberRequest;
+import com.hojoon.mysteam.util.CookieUtil;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+  private final JwtProvider jwtProvider;
+  private final CookieUtil cookieUtil;
+
+  public JwtAuthenticationFilter(AuthenticationManager authenticationManager,
+      JwtProvider jwtProvider, CookieUtil cookieUtil) {
+    super(authenticationManager);
+    this.jwtProvider = jwtProvider;
+    this.cookieUtil = cookieUtil;
+  }
+
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest request,
+      HttpServletResponse response) throws AuthenticationException {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    SignInMemberRequest signInMember = null;
+    try {
+      signInMember = objectMapper.readValue(request.getInputStream(), SignInMemberRequest.class);
+      log.info("User Email is: {}", signInMember.getEmail());
+    } catch (Exception e) {
+      log.error("error: {}", e.getMessage());
+    }
+    UsernamePasswordAuthenticationToken authenticationToken =
+        new UsernamePasswordAuthenticationToken(
+            signInMember.getEmail(), signInMember.getPassword());
+
+    return getAuthenticationManager().authenticate(authenticationToken);
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+      FilterChain chain, Authentication authResult) throws IOException, ServletException {
+    Cookie accessToken = cookieUtil.createCookie(
+        JwtExpiration.ACCESS_TOKEN_EXPIRATION_TIME.getTokenType(),
+        jwtProvider.generateAccessToken(authResult),
+        JwtExpiration.ACCESS_TOKEN_EXPIRATION_TIME.getExpiration());
+
+    Cookie refreshToken = cookieUtil.createCookie(
+        JwtExpiration.REFRESH_TOKEN_EXPIRATION_TIME.getTokenType(),
+        jwtProvider.generateRefreshToken(authResult),
+        JwtExpiration.REFRESH_TOKEN_EXPIRATION_TIME.getExpiration());
+    response.addCookie(accessToken);
+    response.addCookie(refreshToken);
+  }
+}
---
+++ src/main/java/com/hojoon/mysteam/security/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,45 @@
+package com.hojoon.mysteam.security.jwt;
+
+import com.hojoon.mysteam.util.CookieUtil;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+  private final JwtProvider jwtProvider;
+  private final CookieUtil cookieUtil;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request,
+      HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    String token = resolveToken(request, JwtExpiration.ACCESS_TOKEN_EXPIRATION_TIME.getTokenType());
+    if (token != null && jwtProvider.validateToken(token)) {
+      Authentication authentication = jwtProvider.getAuthentication(token);
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+      log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(),
+          request.getRequestURI());
+    } else {
+      log.info("유효한 JWT 토큰이 없습니다, uri: {}", request.getRequestURI());
+    }
+    filterChain.doFilter(request, response);
+  }
+
+  private String resolveToken(HttpServletRequest request, String tokenName) {
+    Cookie cookie = cookieUtil.getCookie(request, tokenName);
+    return cookie != null ? cookie.getValue() : null;
+  }
+}
