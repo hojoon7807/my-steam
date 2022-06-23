d9f5faa5f7d89607c2277dbf919fedd1a5b4ab09
hojoon <ujuj0202@gmail.com>
2022-06-23 23:28:43 +0900

    [MYS-26] member UserDetailsService 구현

--- src/main/java/com/hojoon/mysteam/security/service/CustomMemberDetailsService.java
+++ src/main/java/com/hojoon/mysteam/security/service/CustomMemberDetailsService.java
@@ -1,8 +1,11 @@
-package com.hojoon.mysteam.config.security;
+package com.hojoon.mysteam.security.service;
 
 import com.hojoon.mysteam.member.domain.model.Member;
+import com.hojoon.mysteam.member.domain.repository.FindMemberPort;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,12 +15,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+@Qualifier("memberDetailsService")
+@Slf4j
+public class CustomMemberDetailsService implements UserDetailsService {
+
+  private final FindMemberPort findMemberPort;
 
   @Override
-  public UserDetails loadUserByUsername(
-      String username) throws UsernameNotFoundException {
-    return null;
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    log.info("userDetailsService is run");
+
+    return findMemberPort.findMemberByEmail(username)
+        .map(this::createUserDetails)
+        .orElseThrow(() ->
+            new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
   }
 
   private UserDetails createUserDetails(Member member) {
