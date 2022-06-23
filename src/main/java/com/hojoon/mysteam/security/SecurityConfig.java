15125f9d98c80d26d7e941a66c829fe432c25613
hojoon <ujuj0202@gmail.com>
2022-06-23 23:29:28 +0900

    [MYS-26] sercurity, jwt config 추가

--- src/main/java/com/hojoon/mysteam/security/SecurityConfig.java
+++ src/main/java/com/hojoon/mysteam/security/SecurityConfig.java
@@ -1,10 +1,16 @@
-package com.hojoon.mysteam.config.security;
+package com.hojoon.mysteam.security;
 
+import com.hojoon.mysteam.security.jwt.JwtAccessDeniedHanler;
+import com.hojoon.mysteam.security.jwt.JwtEntryPoint;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +21,10 @@ public class SecurityConfig {
 
   private final JwtEntryPoint jwtEntryPoint;
   private final JwtAccessDeniedHanler jwtAccessDeniedHanler;
+  private final JwtConfig jwtConfig;
+
+  @Qualifier("memberDetailsService")
+  private final UserDetailsService userDetailsService;
 
   @Bean
   public PasswordEncoder passwordEncoder() {
@@ -23,6 +33,7 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
     http
         .httpBasic().disable()
         .cors()
@@ -34,12 +45,23 @@ public class SecurityConfig {
         .accessDeniedHandler(jwtAccessDeniedHanler)
 
         .and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        .and()
         .authorizeRequests()
-        .antMatchers("/", "/signup").permitAll()
+        .antMatchers("/", "/signup", "/signin", "/login").permitAll()
         .anyRequest().authenticated()
 
         .and()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        .apply(jwtConfig);
     return http.build();
   }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration
+  ) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
 }
