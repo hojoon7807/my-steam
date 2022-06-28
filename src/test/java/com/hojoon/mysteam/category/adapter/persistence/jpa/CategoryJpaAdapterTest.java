2c0db80bdc9cd7fefcea2f975f085625aef4fae9
hojoon <ujuj0202@gmail.com>
2022-06-28 19:50:02 +0900

    [MYS-30] Category 추가 기능 테스트 코드 작성

---
+++ src/test/java/com/hojoon/mysteam/category/adapter/persistence/jpa/CategoryJpaAdapterTest.java
@@ -0,0 +1,34 @@
+package com.hojoon.mysteam.category.adapter.persistence.jpa;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.hojoon.mysteam.category.domain.model.Category;
+import com.hojoon.mysteam.common.TCDataJpaTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+
+@TCDataJpaTest
+@Slf4j
+@Import({CategoryJpaAdapter.class, CategoryMapper.class})
+class CategoryJpaAdapterTest {
+
+  @Autowired
+  private CategoryJpaAdapter categoryJpaAdapter;
+
+  @Test
+  @DisplayName("카테고리 정보가 유효하면 해당 객체를 성공적으로 저장하고 반환한다")
+  void saveNewMember() {
+    final String CATEGORY_NAME = "action";
+    Category category = new Category(null, CATEGORY_NAME);
+
+    Category savedCategory = categoryJpaAdapter.saveCategory(category);
+    log.info("category id: {}", savedCategory.getId());
+
+    assertAll(() -> assertThat(savedCategory).isNotNull(),
+        () -> assertThat(savedCategory.getCategoryName()).isEqualTo(CATEGORY_NAME));
+  }
+}
