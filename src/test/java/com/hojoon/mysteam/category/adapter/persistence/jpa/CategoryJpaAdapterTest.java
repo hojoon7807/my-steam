87b876fb36d27f3b4b02759a66c2435e37355bd5
hojoon <ujuj0202@gmail.com>
2022-07-16 02:33:43 +0900

    [MYS-38] CategoryAdapter findAll 테스트 추가

--- src/test/java/com/hojoon/mysteam/category/adapter/persistence/jpa/CategoryJpaAdapterTest.java
+++ src/test/java/com/hojoon/mysteam/category/adapter/persistence/jpa/CategoryJpaAdapterTest.java
@@ -4,17 +4,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.hojoon.mysteam.category.domain.model.Category;
+import com.hojoon.mysteam.common.AbstractContainerBaseTest;
 import com.hojoon.mysteam.common.TCDataJpaTest;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
 @TCDataJpaTest
 @Slf4j
 @Import({CategoryJpaAdapter.class, CategoryMapper.class})
-class CategoryJpaAdapterTest {
+class CategoryJpaAdapterTest extends AbstractContainerBaseTest {
 
   @Autowired
   private CategoryJpaAdapter categoryJpaAdapter;
@@ -26,9 +29,25 @@ class CategoryJpaAdapterTest {
     Category category = new Category(CATEGORY_NAME);
 
     Category savedCategory = categoryJpaAdapter.saveCategory(category);
-    log.info("category id: {}", savedCategory.getId());
 
     assertAll(() -> assertThat(savedCategory).isNotNull(),
         () -> assertThat(savedCategory.getCategoryName()).isEqualTo(CATEGORY_NAME));
+  }
+
+  @Test
+  @DisplayName("DB에 카테고리 정보가 존재하지 않으면 Empty List를 반환한다")
+  void findAllCategoriesReturnEmptyList() {
+    List<Category> categories = categoryJpaAdapter.findAllCategories();
+
+    assertThat(categories).isEmpty();
+  }
+
+  @Test
+  @DisplayName("모든 카테고리 정보를 조회하고 List로 반환한다")
+  @Sql("CategoryJpaAdapterTest.sql")
+  void findAllCategories() {
+    List<Category> categories = categoryJpaAdapter.findAllCategories();
+
+    assertThat(categories).hasSize(2);
   }
 }
---
+++ src/test/resources/com/hojoon/mysteam/category/adapter/persistence/jpa/CategoryJpaAdapterTest.sql
@@ -0,0 +1,5 @@
+insert into category (category_name)
+value ("HORROR");
+
+insert into category (category_name)
+  value ("ACTION");
