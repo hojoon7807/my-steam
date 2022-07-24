24139c0e02eae5b64de85cd407a5671f77f4db94
hojoon <ujuj0202@gmail.com>
2022-07-25 02:08:19 +0900

    [MYS-40] path 유효성 검사 추가
    1이상의 path 값만 유효하다. 통합테스트에서 메소드 테스트

--- src/main/java/com/hojoon/mysteam/category/adapter/rest/controller/DeleteCatgoryController.java
+++ src/main/java/com/hojoon/mysteam/category/adapter/rest/controller/DeleteCatgoryController.java
@@ -1,20 +1,23 @@
 package com.hojoon.mysteam.category.adapter.rest.controller;
 
 import com.hojoon.mysteam.category.application.usecase.DeleteCategoryUsecase;
+import javax.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class DeleteCatgoryController {
 
   private final DeleteCategoryUsecase deleteCategoryUsecase;
 
   @DeleteMapping("categories/{categoryId}")
-  public ResponseEntity deleteCategory(@PathVariable Long categoryId) {
+  public ResponseEntity deleteCategory(@Min(1) @PathVariable("categoryId") Long categoryId) {
     deleteCategoryUsecase.accept(categoryId);
 
     return ResponseEntity.noContent().build();
